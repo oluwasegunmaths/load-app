dfe5ea290b68d9a3cbbb6f23ad4f027bec65567b
Oluwasegun Wahaab <abrolu@yahoo.com>
2021-04-19 11:32:57 +0100

    update comment added 'the'

--- starter/app/src/main/java/com/udacity/LoadingButton.kt
+++ starter/app/src/main/java/com/udacity/LoadingButton.kt
@@ -34,7 +34,7 @@ class LoadingButton @JvmOverloads constructor(
     private val textWidth: Float by lazy { paint.measureText("We are loading") }
 
     private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
-        // Paint styles used for rendering are initialized here. This
+        // The Paint styles used for rendering are initialized here. This
         // is a performance optimization, since onDraw() is called
         // for every screen refresh.
         style = Paint.Style.FILL
